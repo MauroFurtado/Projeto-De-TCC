ca7c4831c9b620350bb78ec54ed1f60d43f5d602
Mauro Furtado <maurofurtado39@gmail.com>
2025-10-19 16:24:51 -0300

    fix: update database connection variables to match environment configuration

--- node_api/ReservaDeSala_API/src/config/db.js
+++ node_api/ReservaDeSala_API/src/config/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const sequelize = new Sequelize
 (
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: 'postgres'
