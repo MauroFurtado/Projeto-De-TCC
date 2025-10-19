f15125bf8a758c98e199c28247e4cfefce184d94
Mauro Furtado <maurofurtado39@gmail.com>
2025-10-19 16:27:15 -0300

    refactor: reorganize imports and enhance Usuario model structure

--- node_api/ReservaDeSala_API/src/models/UsuarioModel.js
+++ node_api/ReservaDeSala_API/src/models/UsuarioModel.js
@@ -1,11 +1,15 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
 
-import {DataTypes} from 'sequelize';
-import {sequelize} from '../config/db.js';
 const Usuario = sequelize.define('Usuario', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
     email: {
         type: DataTypes.STRING,
@@ -15,6 +19,27 @@ const Usuario = sequelize.define('Usuario', {
     senha: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    perfil: {
+        type: DataTypes.ENUM('admin', 'comum'),
+        allowNull: false,
+        defaultValue: 'comum'
+    },
+    criado_em: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
     }
+}, {
+    tableName: 'usuarios',
+    timestamps: false
 });
+
+// associações (chamar Usuario.associate(models) após carregar todos os modelos)
+Usuario.associate = (models) => {
+    if (models.Reserva) {
+        Usuario.hasMany(models.Reserva, { foreignKey: 'usuario_id', as: 'reservas' });
+    }
+};
+
 export default Usuario;
