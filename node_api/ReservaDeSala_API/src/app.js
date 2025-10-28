98964b158e6cd9718347567843a4647850c6480f
Mauro Furtado <maurofurtado39@gmail.com>
2025-10-27 23:04:40 -0300

    feat: refactor app.js to use ES6 module syntax and update middleware and route structure

--- node_api/ReservaDeSala_API/src/app.js
+++ node_api/ReservaDeSala_API/src/app.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes/UsuarioRoutes');
-const middleware = require('./middleware/index');
+import express from 'express';
+import dotenv from 'dotenv';
+import UsuarioRoute from './routes/UsuarioRoutes.js';
+import SalaRoute from './routes/SalaRoutes.js';
+import ReservaRoute from './routes/ReservaRoutes.js';
+
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(middleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use('/api', routes);
+// Routes (mount)
+app.use('/usuarios', UsuarioRoute);
+app.use('/salas', SalaRoute);
+app.use('/reservas', ReservaRoute);
 
 // Start the server
 app.listen(PORT, () => {
