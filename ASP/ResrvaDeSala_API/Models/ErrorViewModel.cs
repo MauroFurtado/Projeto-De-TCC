d4be52ddd2d2918d3f09a29de15af9ea67aedec6
Mauro Furtado <maurofurtado39@gmail.com>
2025-12-01 01:14:49 -0300

    Configuração inicial do ASP.NET Core com EF e Swagger
    Adicionado suporte para:
    - Variáveis de ambiente com `dotenv.net`.
    - Entity Framework Core com PostgreSQL.
    - AutoMapper para mapeamento de objetos.
    - Documentação da API com Swagger/OpenAPI.
    
    Configurações adicionais:
    - Pipeline de requisições HTTP para desenvolvimento e produção.
    - Suporte para controladores e autorização.
    - Inicialização do aplicativo com `app.Run()`.

---
+++ ASP/ResrvaDeSala_API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResrvaDeSala_API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP/ResrvaDeSala_API/Program.cs
@@ -0,0 +1,64 @@
+using dotenv.net;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ResrvaDeSala_API.Data;
+
+DotEnv.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var conn = builder.Configuration.GetConnectionString("DefaultConnection")
+               ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+    if (string.IsNullOrWhiteSpace(conn))
+    {
+        throw new InvalidOperationException("Connection string 'DefaultConnection' not found in configuration or env vars.");
+    }
+
+    options.UseNpgsql(conn);
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        options.RoutePrefix = string.Empty; // Set the Swagger UI at the app's root
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
